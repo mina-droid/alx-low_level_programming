5443a199c3c8e65fdf5b3387bbf23a7bec11ed93
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-09 01:27:18 +0200

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
-fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+char str[60]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+fwrite(str,59,1,stderr);
 return (1);
 }
