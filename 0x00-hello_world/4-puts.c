f2147beeb23511f5631fccade7fe713474361bf2
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-09 00:35:56 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
-char str[51] = "Programming is like building a multilingual puzzle";
+char str[52] = "\"Programming is like building a multilingual puzzle";
 puts(str);
 return (0);
 }
