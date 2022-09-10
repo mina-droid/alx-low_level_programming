1f07d62d62eafaf1fc48b26fe0b25412eb37d8dc
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-10 03:46:19 +0200

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ int main(void)
   }
    else if ((n % 10) == 0)
   {
-    printf("%d and is 0\n",(n % 10) )
+    printf("%d and is 0\n",(n % 10));
   }
    else if ((n % 10) < 6 && (n % 10) != 0)
   {
