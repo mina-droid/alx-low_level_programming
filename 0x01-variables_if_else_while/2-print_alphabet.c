25d8073331506a516929b2d0427f5c31f85e7297
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-10 04:01:44 +0200

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,9 +8,11 @@
 
 int main(void)
 {
-  char ch;
-  for(ch = 'a' ; ch <= 'z' ; ch++) {
-      putchar(ch);
-   }
-  return (0);
+char ch;
+for(ch = 'a' ; ch <= 'z' ; ch++) 
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
 }
