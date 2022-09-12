3d82fe17a1439f4df6a2ea7a39d6227d3532ddb3
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-12 20:25:28 +0200

    Task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i;
+int j;
+for (i = 0; i < 10; i++)
+{
+if (i == 9)
+{
+break;
+}
+for (j = i + 1; j < 10; j++)
+{
+  putchar(i + '0');
+  putchar(j + '0');
+  putchar(',');
+  putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
