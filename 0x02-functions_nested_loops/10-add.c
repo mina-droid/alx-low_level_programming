b6fae1005aef2fc1a9530f3e78df49835cb83c93
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-14 00:20:36 +0200

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
 /**
-* add - checks if a number is positive or negative or zero
+* add - adds two numbers
 *
-* Parameters: TInteger
+* Parameters: two intgers
 *
-* @n: int
+* @a: Number 1
+*
+* @b: Number 2
 *
 * Return: (1) if it is Positive (0) if it is zero (-) otherwise
 */
