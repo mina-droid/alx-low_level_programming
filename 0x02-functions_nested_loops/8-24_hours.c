fc35d764a76faa3e553618eec1f603c6ece230a1
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-13 23:08:03 +0200

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,7 +15,7 @@ int first_nums;
 int second_nums;
 for (first_nums = 0; first_nums < 24 ; first_nums++)
 {
-for (second_nums = 1 ; second_nums < 60; second_nums++)
+for (second_nums = 0 ; second_nums < 60; second_nums++)
 {
 _putchar (first_nums / 10 + '0');
 _putchar (first_nums % 10 + '0');
