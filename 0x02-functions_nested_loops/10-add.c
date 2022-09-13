23a38f200216d671c930dbf92675db34a0331f31
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-14 00:19:13 +0200

    Create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* add - checks if a number is positive or negative or zero
+*
+* Parameters: TInteger
+*
+* @n: int
+*
+* Return: (1) if it is Positive (0) if it is zero (-) otherwise
+*/
+
+int add(int a, int b)
+{
+  return (a + b);
+}
