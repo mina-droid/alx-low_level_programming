e8e131bb2ada3c07bd5da993579ef9d3c5ec4531
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-13 19:18:05 +0200

    Task-0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,2 +1,19 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char *ch = '_putchar';
+while (*ch != '\0')
+{
+ putchar(*ch);
+ ch++;
+}  
+putchar('\n');
+return (0);
+}
