7a1cabfe30c5fb11c0ebfb7596bfa265aff25a8d
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-13 20:04:42 +0200

    Create 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - Prints alphabets 10 times
+*
+*
+*
+* Return void
+*/
+
+void print_alphabet_x10(void)
+{
+ for (int i = 0; i < 11 ; i++)
+ {
+  print_alphabet(); 
+ }
+}
