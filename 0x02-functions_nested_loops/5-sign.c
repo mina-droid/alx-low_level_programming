c5c11d6467a3e48afe48b430a02fd21a74fd454d
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-13 21:47:48 +0200

    Create 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* print_sign - checks if a number is positive or negative or zero
+*
+* Parameters: TInteger
+*
+* @n: int
+*
+* Return: (1) if it is Positive (0) if it is zero (-) otherwise
+*/
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else 
+{
+_putchar('-');
+return (-1);
+}
+}
