806a873d43f4a1c6664d5b243041bdf5a15118b7
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-14 00:29:29 +0200

    Create 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_to_98 - adds two numbers
+*
+* Parameters: one intger
+*
+* @n: Start Number
+*
+* Return: result
+*/
+
+print_to_98(int n)
+{
+int i;
+if (n < 98)
+{
+for (i = n; i < 99; i++)
+{
+putchar  
+}  
+}
+
+}
