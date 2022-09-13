8d4e3b7c6448a907a6bb5602a659cac6668d82ed
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-13 23:30:01 +0200

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,13 +14,12 @@ void times_table(void)
 int tArr[10][10];
 int i;
 int j;
-int t;
 for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 10; j++)
 {
-t = (i * j);
-_putchar(t + '0');
+tArr[i][j] = (i * j);
+_putchar(tArr[i][j] + '0');
 }
 _putchar ('\n');
 }
