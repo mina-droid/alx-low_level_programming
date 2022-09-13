f918c2717603ff7ffeb9812a5a789c5dd82112b9
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-14 00:32:29 +0200

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,31 @@
 #include "main.h"
 
 /**
+* print_last_digit - prints the last digit of a number
+*
+* Parameters: Integer
+*
+* @n: int
+*
+* Return: (lD) the last digit of a number
+*/
+
+int  print_last_digit(int n)
+{
+if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else if (n / 10)
+{
+print_last_digit(n / 10);
+}
+_putchar(n % 10 + '0');
+return (0);
+}
+
+/**
 * print_to_98 - adds two numbers
 *
 * Parameters: one intger
@@ -17,8 +42,18 @@ if (n < 98)
 {
 for (i = n; i < 99; i++)
 {
-putchar  
+print_last_digit(n); 
 }  
 }
-
+else if (n > 98)
+{
+for (i = n; i > 97; i--)
+{
+print_last_digit(n); 
+}  
+}
+else
+{
+print_last_digit(98);
+}
 }
