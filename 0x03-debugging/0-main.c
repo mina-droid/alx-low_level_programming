5bc2f7f3321fc840302061d4f81576911963c080
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-14 18:32:05 +0200

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,12 @@
 #include "main.h"
 /**
-* positive_or_negative -  prints if integer is positive or negative
+* main -  prints if integer is positive or negative
 * Return: 0
 */
+
+int main(void)
+{
 positive_or_negative(0);
+return (0);
+}
+
