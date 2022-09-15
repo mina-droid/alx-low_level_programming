2e1c1585860dfee714fcb73eb41a248125c5a615
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-16 01:32:16 +0200

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,17 +6,17 @@
  */
 void print_diagonal(int n)
 {
-	int i = 0;
+	int i = 0, j;
 	while (i < n && n > 0)
 	{
-		int j = 0;
-		_putchar('\\');
-                _putchar('\n');
+		j = 0;
 		while (j <= i)
 		{
 			_putchar(' ');
 			j++;
 		}
+		_putchar('\\');
+                _putchar('\n');
 		i++;
 	}
 	_putchar('\n');
