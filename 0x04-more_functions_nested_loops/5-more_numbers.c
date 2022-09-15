6a009d947d242d6b6413529e449d7e3acf0ef4fd
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-16 00:49:41 +0200

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -28,16 +28,26 @@ return (0);
  */
 void more_numbers(void)
 {
-int i = 0;
-int j = 0;
-while (i < 10)
-{
-while (j < 15)
-{
-print_last_digit(j);
-j++;
-}
-_putchar('\n');
-i++;
-}
+	char n, c;
+	int i = 0;
+
+
+	while (i < 10)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			c = n;
+			if (n > 9)
+			{
+				_putchar('1');
+				c = n % 10;
+			}
+
+			_putchar('0' + c);
+		}
+
+		_putchar('\n');
+		i++;
+	}
+
 }
