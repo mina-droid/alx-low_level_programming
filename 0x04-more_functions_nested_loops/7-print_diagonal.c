fd52107e9bce8f517b6d69e1c2d90c9b797fb213
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-16 01:14:56 +0200

    Create 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_diagonal - prints numbers
+ * @n: points
+ * Return:0
+ */
+void print_diagonal(int n)
+{
+int i = 0;
+
+	while (i < n && n > 0)
+	{
+		_putchar('\');
+    _putchar('\n');
+    _putchar(' ');
+		i++;
+	}
+	_putchar('\n');
+}
