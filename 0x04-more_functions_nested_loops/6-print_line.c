e91ef8601f0441f4fbda3f06da99026fa27054fa
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-16 01:07:35 +0200

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,19 +6,12 @@
  */
 void print_line(int n)
 {
- if (n <= 0)
- {
-  _putchar('\n');
- }
- else
- {
 int i = 0;
 
-	while (i < n)
+	while (i < n && n > 0)
 	{
 		_putchar('_');
 		i++;
 	}
 	_putchar('\n');
- }
 }
