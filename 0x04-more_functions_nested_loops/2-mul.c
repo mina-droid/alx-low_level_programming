5784c6659d3e95a8e0a3b204c201efe31b249986
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-16 00:06:19 +0200

    Create 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multipliy two numbers
+ *
+ * @a: number 1
+ * @b: number 2
+ * Return: 1 if uppercase 0 otherwise .
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
