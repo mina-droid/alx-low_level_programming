728614104e7d9684c6e90308ca1a2ef43391e5d6
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-16 01:49:34 +0200

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
  * print_square - check for a digit
- * @n : number of \\ to be printed
+ * @n : number of _ to be printed
  * Return:void
  */
 
-void print_square(int size)
+void print_square(int n)
 {
 
 	int i = 0, ii;
 
-	while (i < size && size > 0)
+	while (i < n && n > 0)
 	{
 		ii = 0;
-		while (ii < size)
+		while (ii < n)
 		{
 			_putchar('#');
 			ii++;
@@ -24,6 +24,5 @@ void print_square(int size)
 	}
 	if (i == 0)
 		_putchar('\n');
-
 
 }
