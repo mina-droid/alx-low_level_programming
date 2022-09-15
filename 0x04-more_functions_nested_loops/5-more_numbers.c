b1a7468c493ee54b13b5e8147510ca28da0f5d4d
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-16 00:50:07 +0200

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,28 +1,5 @@
 #include "main.h"
 /**
-* print_last_digit - prints the last digit of a number
-*
-* Parameters: Integer
-*
-* @n: int
-*
-* Return: (lD) the last digit of a number
-*/
-int  print_last_digit(int n)
-{
-if (n == 0)
-{
-_putchar('0');
-return (0);
-}
-else if (n / 10)
-{
-print_last_digit(n / 10);
-}
-_putchar(n % 10 + '0');
-return (0);
-}
-/**
  * more_numbers - prints numbers
  * Return:0
  */
