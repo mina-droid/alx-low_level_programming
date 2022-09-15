8a8cd561d5ad3bfe9df897bfe7cf1cd2b3b3300b
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-16 00:29:41 +0200

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,20 +1,23 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints numbers from 0 to 9 except 2 and 4 
- *
- * Return: 0.
+ * print_most_numbers - prints numbers
+ * Return:0
  */
 
 void print_most_numbers(void)
 {
-int i;
-for (i = 0; i < 10; i++)
-{
-if ((i != 2 && i != 4))
-{
-_putchar(i + '0');
-}
-}
-_putchar('\n');
+	char n = 0;
+
+	while (n <= 9)
+	{
+		if (n != 2 && n != 4)
+		{
+			_putchar('0' + n);
+		}
+
+		n++;
+	}
+	_putchar('\n');
+
 }
