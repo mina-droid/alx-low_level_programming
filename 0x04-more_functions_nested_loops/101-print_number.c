22b7d0d2e78fb2c1490a50787838433ede843158
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-16 02:59:58 +0200

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,3 +1,15 @@
+#include "main.h"
+
+/**
+* print_last_digit - prints the last digit of a number
+*
+* Parameters: Integer
+*
+* @n: int
+*
+* Return: (lD) the last digit of a number
+*/
+
 int  print_last_digit(int n)
 {
 if (n < 0)
@@ -16,4 +28,19 @@ print_last_digit(n / 10);
 }
 _putchar(n % 10 + '0');
 return (0);
+}
+
+/**
+* print_number - prints the last digit of a number
+*
+* Parameters: Integer
+*
+* @n: int
+*
+* Return: (lD) the last digit of a number
+*/
+
+void print_number(int n)
+{
+print_last_digit(n);
 }
