8e40a454c5e31f5443f8dee9b4d65242e3427c65
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-16 02:19:31 +0200

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,36 +1,41 @@
 #include <stdio.h>
 /**
- * main - check for a digit
- * Return: int
+ * main - check the code for Holberton School students.
+ *
+ * Return: void.
  */
 
 int main(void)
 {
-int i;
-for (i = 1; i <= 100; i++)
-{
-if (i % 3 == 0 && i % 5 == 0)
-{
-printf("FizzBuzz");
-}
-else if (i % 3 == 0)
-{
-printf("Fizz");
-}
-else if (i % 5 == 0)
-{
-printf("Buzz");
-}
-else
-{
-printf("%i", i);
-}
-if (i != 100)
-{
-putchar(' ');
-}
-}
-putchar('\n');
-return (0);
-  
+
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%i", i);
+		}
+
+		if (i != 100)
+		{
+			putchar(' ');
+		}
+
+	}
+	putchar('\n');
+	return (0);
+
 }
