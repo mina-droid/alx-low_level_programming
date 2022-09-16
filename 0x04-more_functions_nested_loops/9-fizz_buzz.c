70b6e9df5331824543768cf9046550a43156fdfd
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-16 02:14:31 +0200

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,23 +12,24 @@ for (i = 1; i <= 100; i++)
 if (i % 3 == 0 && i % 5 == 0)
 {
 printf("FizzBuzz");
-putchar(' ');
 }
 else if (i % 3 == 0)
 {
 printf("Fizz");
-putchar(' ');
 }
 else if (i % 5 == 0)
 {
 printf("Buzz");
-putchar(' ');
 }
 else
 {
 printf("%d", i);
+}
+if (i != 100)
+{
 putchar(' ');
 }
 }
+putchar('\n');
 return(0);
 }
