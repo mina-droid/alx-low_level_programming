5e6396f3735a4ce3a0afc28caa683b006f1040d5
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-20 21:04:01 +0200

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -27,19 +27,34 @@ return (0);
 
 int _atoi(char *s)
 {
-int res = 0;
-int sign = 1;
-int i = 0;
-for (; s[i] != '\0'; i++)
-{
-if (_isdigit(s[i]))
-{
-if (s[i - 1] == '-')
-{
-sign = -1;
-}
-res = res * 10 + s[i] - '0';
-}
-}
-return (sign * res);
+int sign = 1, base = 0, i = 0;
+     
+    // if whitespaces then ignore.
+    while (s[i] == ' ')
+    {
+        i++;
+    }
+     
+    // sign of number
+    if (s[i] == '-' || s[i] == '+')
+    {
+        sign = 1 - 2 * (s[i++] == '-');
+    }
+   
+    // checking for valid input
+    while (s[i] >= '0' && s[i] <= '9')
+    {
+        // handling overflow test case
+        if (base > INT_MAX / 10
+            || (base == INT_MAX / 10
+            && s[i] - '0' > 7))
+        {
+            if (sign == 1)
+                return INT_MAX;
+            else
+                return INT_MIN;
+        }
+        base = 10 * base + (s[i++] - '0');
+    }
+    return base * sign;
 }
