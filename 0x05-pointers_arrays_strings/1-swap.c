858a5b305bcea38172a0c8fe4dce8e5d6b7b7a37
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-20 02:17:26 +0200

    Create 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* reset_to_98-takes a pointer to an int and updates its value to 98
+*
+* @n: a pointer to an int
+*
+* Return: void
+*/
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
