c527e1ca54fb2a6b18977c2d6687e65db1ceeb43
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-20 20:52:05 +0200

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,35 +1,4 @@
 #include "main.h"
-
-/**
-* print_last_digit - prints the last digit of a number
-*
-* Parameters: Integer
-*
-* @n: int
-*
-* Return: (lD) the last digit of a number
-*/
-
-int  print_last_digit(int n)
-{
-if (n < 0)
-{
-_putchar('-');
-n = -n;
-}
-if (n == 0)
-{
-_putchar('0');
-return (0);
-}
-else if (n / 10)
-{
-print_last_digit(n / 10);
-}
-_putchar(n % 10 + '0');
-return (0);
-}
-
 /**
  * _isdigit - checks for upper case charchter
  *
