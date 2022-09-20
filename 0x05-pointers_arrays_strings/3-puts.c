0ff05681963ec7c85b6102446b260f9b055a84a8
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-20 02:38:44 +0200

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+* _strlen-swaps the values of two integers
+*
+* @s: string
+*
+* Return: int
+*/
+
+int _strlen(char *s)
+{
+int count = 0;
+while (*s != '\0')
+{
+count++;
+s++;
+}
+return (count);
+}
+
+/**
 * _puts-prints a string followed by a new line, to stdout
 *
 * @str: string
