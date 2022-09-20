c3c00bbceded1fad7ab06f1ddec8764bf176acef
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-20 21:06:14 +0200

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 /**
  * _isdigit - checks for upper case charchter
  *
@@ -28,33 +29,26 @@ return (0);
 int _atoi(char *s)
 {
 int sign = 1, base = 0, i = 0;
-     
-    // if whitespaces then ignore.
-    while (s[i] == ' ')
-    {
-        i++;
-    }
-     
-    // sign of number
-    if (s[i] == '-' || s[i] == '+')
-    {
-        sign = 1 - 2 * (s[i++] == '-');
-    }
-   
-    // checking for valid input
-    while (s[i] >= '0' && s[i] <= '9')
-    {
-        // handling overflow test case
-        if (base > INT_MAX / 10
-            || (base == INT_MAX / 10
-            && s[i] - '0' > 7))
-        {
-            if (sign == 1)
-                return INT_MAX;
-            else
-                return INT_MIN;
-        }
-        base = 10 * base + (s[i++] - '0');
-    }
-    return base * sign;
+while (s[i] == ' ')
+{
+i++;
+}
+if (s[i] == '-' || s[i] == '+')
+{
+sign = 1 - 2 * (s[i++] == '-');
+}
+while (s[i] >= '0' && s[i] <= '9')
+{
+if (base > INT_MAX / 10
+|| (base == INT_MAX / 10
+&& s[i] - '0' > 7))
+{
+if (sign == 1)
+return INT_MAX;
+else
+return INT_MIN;
+}
+base = 10 * base + (s[i++] - '0');
+}
+return base * sign;
 }
