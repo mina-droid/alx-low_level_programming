7ebec3bc9e6ecb6fad409ba6f5859213ed13d652
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-20 21:33:02 +0200

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,21 +8,29 @@
 
 int _atoi(char *s)
 {
-	int sign = 1, resp = 0, firstNum,i;
+	int i;
+	int h, p;
 
-	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
+	h = 0;
+	p = -1;
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[firstNum] == '-')
+		if (s[i] == '-')
+			p *= -1;
+
+		if (s[i] > 47 && s[i] < 58)
 		{
-			sign *= -1;
+			if (h < 0)
+				h = (h * 10) - (s[i] - '0');
+			else
+				h = (s[i] - '0') * -1;
+
+			if (s[i + 1] < 48 || s[i + 1] > 57)
+				break;
 		}
 	}
+	if (p < 0)
+		h *= -1;
 
-	for (i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
-	{
-		resp *= 10;
-		resp += (s[i] - 48);
-	}
-
-	return (sign * resp);
+	return (h);
 }
