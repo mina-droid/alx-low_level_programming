b1d8e0aa392fb48f5f2933359f0f64fb0171fc8e
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-20 20:42:05 +0200

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,22 @@
 #include "main.h"
 
+/**
+ * _isdigit - checks for upper case charchter
+ *
+ * @c: the ascii number of the digit
+ * Return: 1 if digit 0 otherwise .
+ */
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
 
 /**
 * _atoi-prints a string followed by a new line, to stdout
@@ -11,5 +28,11 @@
 
 int _atoi(char *s)
 {
-while
+while (*s != '\0')
+{
+if (_isdigit(*s) && *(s--) == '-')
+{
+_putchar
+}
+}
 }
