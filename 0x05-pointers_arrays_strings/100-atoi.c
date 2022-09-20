be1c636c6181a1fa06231bf98d7495c278826bc9
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-20 20:53:36 +0200

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -31,12 +31,12 @@ int res = 0;
 int sign = 1;
 int i = 0;
 for (; s[i] != '\0'; i++)
+{
 if (_isdigit(s[i]) && s[i - 1] == '-')
 {
 sign = -1;
 }
 res = res * 10 + s[i] - '0';
 return sign * res;
-}
 }
 }
