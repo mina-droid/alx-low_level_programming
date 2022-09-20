068d30edbbb40fbd0a0c9d4f3c8a86b2fe80e0f3
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-20 04:10:52 +0200

    Create 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+* _strcpy-prints a string followed by a new line, to stdout
+*
+* @dest: destintion
+*
+* @src: sourc
+*
+* Return: void
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+dest = &src;
+return (dest);
+}
