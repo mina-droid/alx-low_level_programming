17283220240baccda1a39f624f921cfec81c271c
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-21 23:45:24 +0200

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+* _strlen-swaps the values of two integers
+*
+* @s: string
+*
+* Return: int
+*/
+
+int _strlen(char *s)
+{
+int count = 0;
+while (*s != '\0')
+{
+count++;
+s++;
+}
+return (count);
+}
+
+/**
  *_strcat - convert a string to an integer.
  * @dest: string one
  *
