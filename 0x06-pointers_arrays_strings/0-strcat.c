30751947e405d8f0cb234b71843634e91dae2b8b
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-21 23:44:44 +0200

    Create 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_strcat - convert a string to an integer.
+ * @dest: string one
+ *
+ * @src: string two
+ *
+ * Return: string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	
+}
