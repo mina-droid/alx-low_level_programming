9052fbb085c7d2665d43c745ec36d765d8e880d9
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-22 05:44:45 +0200

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -28,18 +28,15 @@ return (count);
  * Return: string
  */
 
-int _strcmp(char *s1, char *s2)
-{
-int i = 0;
-int res = 0;
-while (s1[i] != '\0' && s2[i] != '\0')
-{
-if (s1[i] != s2[i])
-{
-res = (s1[i] - s2[i]);
-break;
-}
-i++;
-}
-return (res);
+int _strcmp(char * s1, char * s2) {
+  int i = 0;
+  int res = 0;
+  while (s1[i] != '\0' && s2[i] != '\0') {
+    if (s1[i] != s2[i]) {
+      res = (s1[i] - s2[i]);
+      break;
+    }
+    i++;
+  }
+  return (res);
 }
