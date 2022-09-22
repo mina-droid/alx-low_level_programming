d15095a52961199e0c476ae8e85672911e350a93
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-22 07:00:32 +0200

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,27 @@
 #include "main.h"
 /**
+* _islower - checks if a character is lowercase or not
+*
+* Parameters: Takes a char or int
+*
+* @c: a char or int
+*
+* Return: (1) if it is lower (0) otherwise
+*/
+
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
+
+/**
 * _isalpha - checks if a character is from the alphabet or not
 *
 * Parameters: Takes a char or int
@@ -29,6 +51,20 @@ return (0);
 
 char *cap_string(char *s)
 {
-
+int i = 0;
+if (_islower(s[i]))
+{
+s[i] = s[i] - 32;
+}
+while (s[i] != '\0')
+{
+if (s[i] == '\n' || s[i] == '.' || s[i] == '!' || s[i] == '?' || s[i] == ';' || s[i] == ',' || s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}' || s[i] == '"')
+{
+if (_isalpha(s[i + 1]) && _islower(s[i + 1]))
+{
+s[i + 1] = s[i + 1] - 32;
+}
+}
+}
 return (s);
 }
