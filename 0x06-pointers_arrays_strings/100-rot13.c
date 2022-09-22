618bb008d96f3c11da5b1f29b13200bd4e8a99ac
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-22 22:55:26 +0200

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,23 +1,29 @@
 #include "main.h"
-
 /**
- *rot13 - convert a string to an integer.
- * @s: string
- * Return: char pointer
+ * rot13 - encodes a string using rot13
+ * @s: input string.
+ * Return: the pointer to dest.
  */
 
 char *rot13(char *s)
 {
-int Rot13[] = {78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109};
-int i = 0;
-int j;
-while (s[i] != '\0')
+int count = 0, i;
+char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+while (*(s + count) != '\0')
 {
-for (j = 0; j < 52; j++)
+
+for (i = 0; i < 52; i++)
 {
+if (*(s + count) == alphabet[i])
+{
+*(s + count) = rot13[i];
+break;
 }
 }
-i++;
+count++;
 }
+
 return (s);
 }
