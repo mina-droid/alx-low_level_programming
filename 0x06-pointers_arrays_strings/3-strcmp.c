e6ee998ea421ac84b7f760502881e91098f8c044
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-22 05:53:44 +0200

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,25 +1,6 @@
 #include "main.h"
 
 /**
-* _strlen-swaps the values of two integers
-*
-* @s: string
-*
-* Return: int
-*/
-
-int _strlen(char *s)
-{
-int count = 0;
-while (*s != '\0')
-{
-count++;
-s++;
-}
-return (count);
-}
-
-/**
  *__strcmp - convert a string to an integer.
  * @s1: string one
  *
