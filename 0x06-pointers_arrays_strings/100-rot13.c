cbf2a2171894fec6a5ab90260c2e7148e3c1a31a
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-22 21:38:33 +0200

    Create 100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *rot13 - convert a string to an integer.
+ * @s: string
+ * Return: char pointer
+ */
+
+char *rot13(char *s)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+if (_isalpha(s[i]))
+{
+s[i] = s[i] + 13;
+}
+}
+return (s);
+}
