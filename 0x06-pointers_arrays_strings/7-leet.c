fef073b7d48cc1dbcbbef86ab6e0008231d696e2
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-22 07:43:16 +0200

    Create 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,21 @@
+/**
+* _islower - checks if a character is lowercase or not
+*
+* Parameters: Takes a char or int
+*
+* @c: a char or int
+*
+* Return: (1) if it is lower (0) otherwise
+*/
+
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
