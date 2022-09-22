0908e80f38de55f83e250690f6e170139f53fd0f
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-22 07:22:31 +0200

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,70 +1,31 @@
 #include "main.h"
 /**
-* _islower - checks if a character is lowercase or not
-*
-* Parameters: Takes a char or int
-*
-* @c: a char or int
-*
-* Return: (1) if it is lower (0) otherwise
-*/
-
-int _islower(int c)
-{
-if (c >= 97 && c <= 122)
-{
-return (1);
-}
-else
-{
-return (0);
-}
-}
-
-/**
-* _isalpha - checks if a character is from the alphabet or not
-*
-* Parameters: Takes a char or int
-*
-* @c: a char or int
-*
-* Return: (1) if it is alphabet (0) otherwise
-*/
-
-int _isalpha(int c)
-{
-if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
-{
-return (1);
-}
-else
-{
-return (0);
-}
-}
-
-/**
- *cap_string - convert a string to an integer.
- * @s: string
- * Return: void
+ * cap_string - capitalizes all words of a string
+ * @s: input string.
+ * Return: the pointer to dest.
  */
+
 
 char *cap_string(char *s)
 {
-int i = 0;
-if (_islower(s[i]))
+int count = 0, i;
+int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+if (*(s + count) >= 97 && *(s + count) <= 122)
+*(s + count) = *(s + count) - 32;
+count++;
+while (*(s + count) != '\0')
 {
-s[i] = s[i] - 32;
+for (i = 0; i < 13; i++)
+{
+if (*(s + count) == separators[i])
+{
+if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+*(s + (count + 1)) = *(s + (count + 1)) - 32;
+break;
 }
-while (s[i] != '\0')
-{
-if (s[i] == '\n' || s[i] == '.' || s[i] == '!' || s[i] == '?' || s[i] == ';' || s[i] == ',' || s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}' || s[i] == '\"' || s[i] == '\t')
-{
-if (_isalpha(s[i + 1]) && _islower(s[i + 1]))
-{
-s[i + 1] = s[i + 1] - 32;
 }
-}
+count++;
 }
 return (s);
 }
