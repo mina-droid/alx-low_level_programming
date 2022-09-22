a6d3c3c6899683bc24d7e701c20fff5792d058fc
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-22 06:46:01 +0200

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,27 @@
 #include "main.h"
 /**
+* _isalpha - checks if a character is from the alphabet or not
+*
+* Parameters: Takes a char or int
+*
+* @c: a char or int
+*
+* Return: (1) if it is alphabet (0) otherwise
+*/
+
+int _isalpha(int c)
+{
+if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
+
+/**
  *cap_string - convert a string to an integer.
  * @s: string
  * Return: void
