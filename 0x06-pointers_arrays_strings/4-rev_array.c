58413bf72346faf5c61eb622eaf4166909c3bd9c
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-22 06:14:59 +0200

    Create 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *reverse_array - convert a string to an integer.
+ * @a: array
+ *
+ * @n: size of array
+ *
+ * Return: string
+ */
+ 
+void reverse_array(int *a, int n)
+{
+int i = 0;
+int s = n - 1;
+int e;
+while (i != s)
+{
+e = a[i];
+a[i] = a[s];
+a[s] = e;
+i++;
+s--;
+}
+}
