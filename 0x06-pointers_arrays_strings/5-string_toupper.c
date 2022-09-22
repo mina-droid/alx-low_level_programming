7bcbfad852fe8150ef883f9ded8bacd41cf0055e
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-22 06:35:07 +0200

    Create 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *string_toupper - convert a string to an integer.
+ * @s: string
+ * Return: void
+ */
+
+char *string_toupper(char * s)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] >= 97 && s[i] <= 122)
+{
+s[i] = s[i] - 32;
+}
+}
+return (s);
+}
