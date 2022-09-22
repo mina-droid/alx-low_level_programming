a9b75de95e3e9404b68b4eab14f89385c347b9e9
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-22 22:41:14 +0200

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,25 +1,5 @@
 #include "main.h"
 
-char *leet(char *s)
-{
-int lowerCase [] = {97, 101, 111, 116, 108};
-int upperCase [] = {65, 69, 79, 84, 76};
-int numbersArr [] = {52, 51, 48, 55, 49};
-int i;
-int j;
-for (i = 0; s[i] != '\0'; i++)
-{
-for (j = 0; j < 5; j++)
-{
-if (s[i] == lowerCase[j] || s[i] == upperCase[j])
-{
-s[i] = numbersArr[j];
-}
-}
-}
-return (s);
-}
-
 /**
  *rot13 - convert a string to an integer.
  * @s: string
@@ -39,8 +19,10 @@ for (j = 0; j < 52; j++)
 if (s[i] == lowerCaseAndUpper[j])
 {
 s[i] = Rot13[j];
+break;
 }
 }
+i++;
 }
 return (s);
 }
