dc3c6318faf78ee9166c748cdf02d384e91bbeeb
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-23 19:23:05 +0200

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,118 +1,65 @@
 #include "main.h"
 
 /**
- *_strcat - convert a string to an integer.
- * @dest: string one
- *
- * @src: string two
- *
- * Return: string
+ * infinite_add - add 2 integers.
+ * @n1: integer
+ * @n2: integer
+ * @r: buffer
+ * size_r: size of r
+ * Return: char
  */
 
-char *_strcat(char *dest, char *src)
+int _atoi(char *s)
 {
-int dest_len = _strlen(dest);
-while (*src != '\0')
-{
-dest[dest_len] = *src;
-dest_len++;
-src++;
-}
-dest[dest_len] = '\0';
-return (dest);
-}
+	int sign = 1, resp = 0, firstNum;
 
-/**
- * rev_string - prints reversed string, followed by a new line
- * @s: pointer to the string to print
- * Return: void
-*/
+	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
+	{
+		if (s[firstNum] == '-')
+		{
+			sign *= -1;
+		}
+	}
 
-void rev_string(char *s)
-{
+	for (int i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
+	{
+		resp *= 10;
+		resp += (s[i] - 48);
+	}
 
-int len, i, half;
-char temp;
-
-for (len = 0; s[len] != '\0'; len++)
-;
-
-i = 0;
-
-half = len / 2;
-
-while (half--)
-{
-	temp = s[len - i - 1];
-	s[len - i - 1] = s[i];
-	s[i] = temp;
-	i++;
+	return (sign * resp);
 }
 
-}
-
-/**
-* _strlen-swaps the values of two integers
-*
-* @s: string
-*
-* Return: int
-*/
-
-int _strlen(char *s)
+void int_to_string(int n)
 {
-int count = 0;
-while (*s != '\0')
+int divisor = 1, i, resp;
+
+
+for (i = 0; n / divisor > 9; i++)
 {
-count++;
-s++;
-}
-return (count);
+	divisor *= 10;
 }
 
-/**
-* infinite_add - prints the last digit of a number
-*
-* Parameters: Integer
-*
-* @n1: big  int1
-*
-* @n2: big int2
-*
-* @r: buffer
-*
-* @size_r: buffer size
-*
-* Return: (lD) the last digit of a number
-*/
+char str[i];
+
+for (int cmpt = 0; divisor >= 10; divisor /= 10, cmpt++)
+{
+	resp = n / divisor;
+	str[cmpt] = '0' + resp;
+	n = n - resp * divisor;
+}
+str[i] = ('0' + n);
+
+}
+
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int nSmall;
-int nstr1 = _strlen(n1);
-int nstr2 = _strlen(n2);
-int carry = 0;
-int sum;
-int i;
-if (nstr1 + nstr2 >= size_r)
-{
-return ('\0');
-}
-if (_strlen(n1) > _strlen(n2))
-{
-nSmall = _strlen(n2);
-}
-else
-{
-nSmall = _strlen(n1);
-}
-rev_string(n1);
-rev_string(n2);
-for (i = 0; i < nSmall; i++)
-{
-sum = ((n1[i] - '0')+(n2[i] - '0') + carry);
-r = _strcat(r, (sum % 10 + '0'));
-carry = sum / 10;
-}
-return (r);
+    int sum, a, b;
+    a = _atoi(n1);
+    b = _atoi(n2);
+
+    sum = a + b;
+
+
 }
