99b8967f8001880bb4d0b5d6ea424127a301f78f
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-23 17:52:40 +0200

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,1 +1,40 @@
-daf
+#include "main.h"
+
+/**
+* print_number - prints the last digit of a number
+*
+* Parameters: Integer
+*
+* @n: int
+*
+* Return: (lD) the last digit of a number
+*/
+
+void print_number(int n)
+{
+unsigned int m, d, count;
+
+if (n < 0)
+{
+_putchar(45);
+m = n * -1;
+}
+else
+{
+m = n;
+}
+
+d = m;
+count = 1;
+
+while (d > 9)
+{
+d /= 10;
+count *= 10;
+}
+
+for (; count >= 1; count /= 10)
+{
+_putchar(((m / count) % 10) + 48);
+}
+}
