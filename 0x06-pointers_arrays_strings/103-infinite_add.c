8bf7ff3fb5e0a8f55d7e8a63b219317f607af47e
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-23 18:05:09 +0200

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,6 +1,35 @@
 #include "main.h"
 
 /**
+ * rev_string - prints reversed string, followed by a new line
+ * @s: pointer to the string to print
+ * Return: void
+*/
+
+void rev_string(char *s)
+{
+
+int len, i, half;
+char temp;
+
+for (len = 0; s[len] != '\0'; len++)
+;
+
+i = 0;
+
+half = len / 2;
+
+while (half--)
+{
+	temp = s[len - i - 1];
+	s[len - i - 1] = s[i];
+	s[i] = temp;
+	i++;
+}
+
+}
+
+/**
 * _strlen-swaps the values of two integers
 *
 * @s: string
@@ -40,6 +69,8 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 int nSmall;
 int nstr1 = _strlen(n1);
 int nstr2 = _strlen(n2);
+int carry = 0;
+int sum;
 if (nstr1 + nstr2 >= size_r)
 {
 return (0);
