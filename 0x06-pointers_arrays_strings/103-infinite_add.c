4496f606ae6e4a748cdd62977f428563a4aaacc9
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-23 18:03:08 +0200

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+* _strlen-swaps the values of two integers
+*
+* @s: string
+*
+* Return: int
+*/
+
+int _strlen(char *s)
+{
+int count = 0;
+while (*s != '\0')
+{
+count++;
+s++;
+}
+return (count);
+}
+
+/**
 * infinite_add - prints the last digit of a number
 *
 * Parameters: Integer
@@ -18,5 +37,19 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-
+int nSmall;
+int nstr1 = _strlen(n1);
+int nstr2 = _strlen(n2);
+if (nstr1 + nstr2 >= size_r)
+{
+return (0);
+}
+if (_strlen(n1) > _strlen(n2))
+{
+nSmall = _strlen(n2);
+}
+else
+{
+nSmall = _strlen(n1);
+}
 }
