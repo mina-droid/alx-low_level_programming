e725caa5a161e7f8e046e9cba2432a655cc483f2
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-26 22:15:06 +0200

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,19 +11,23 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-int j;
-while (*s != '\0')
-{
-j = 0;
-while (accept[j] != '\0')
-{
-if (*s == accept[j])
-{
-return (s);
-}
-j++;
-}
-s++;
-}
-return (0);
+	int j;
+
+	while (*s != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (*s == accept[j])
+			{
+				return (s);
+			}
+
+			j++;
+		}
+
+		s++;
+	}
+	return (0);
+
 }
