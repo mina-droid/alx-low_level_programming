18cc556eced7fe94d29ae2fb29caf25d411ca0df
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-27 00:55:13 +0200

    Create 100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * set_string - encodes a string using rot13
+ * @s: input string.
+ *
+ * @to: input string.
+ *
+ * Return: the pointer to dest.
+ */
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
