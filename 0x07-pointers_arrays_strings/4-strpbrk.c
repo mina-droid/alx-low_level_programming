e05a28cdbc224cb90715bf6ff274996b8fcdd993
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-26 22:16:14 +0200

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,32 +1,30 @@
-    
 #include "main.h"
 
 /**
- * _strpbrk - encodes a string using rot13
- * @s: input string.
- *
- * @accept: input char.
- *
- * Return: the pointer to dest.
- */
+ * _strpbrk - matches any character specified
+ * @s: This is the C string to be scanned.
+ * @accept:character in str1 that matches one of the characters in str2
+ *Return: string s that matches any character specified in accept
+ **/
+
 char *_strpbrk(char *s, char *accept)
 {
 	int j;
 
-	while (*s != '\0')
+	while (*s != '\0') /*Declaring WHILE*/
 	{
 		j = 0;
-		while (accept[j] != '\0')
+		while (accept[j] != '\0')  /*Evaluating *accept*/
 		{
 			if (*s == accept[j])
 			{
 				return (s);
 			}
 
-			j++;
+			j++; /*add j+1*/
 		}
 
-		s++;
+		s++; /*add s+1*/
 	}
 	return (0);
 
