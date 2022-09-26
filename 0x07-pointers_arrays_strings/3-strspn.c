5233a21628aeea4ee81667334b81c8f819916254
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-26 21:36:02 +0200

    Create 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * _strchr - encodes a string using rot13
+ * @s: input string.
+ *
+ * @c: input char.
+ *
+ * Return: the pointer to dest.
+ */
+
+char *_strchr(char *s, char c)
+{
+int i;
+char *oc = '\0';
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] == c)
+{
+oc = &s[i];
+break;
+}
+}
+if (s[i] == c)
+{
+oc = &s[i];
+}
+return (oc);
+}
+
+/**
+ *_strspn - encodes a string using rot13
+ * @s: input string.
+ *
+ * @accept: input string.
+ *
+ * Return: the pointer to dest.
+ */
+
+unsigned int _strspn(char *s, char *accept);
+{
+unsigned int res = 0;
+char *f;
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+f = _strchr(accept, s[i]);
+if (f != NULL)
+{
+res++;
+}
+}
+return (res);
+}
