89fb32e2aef21627d2509b3e08d382630d267469
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-26 19:47:18 +0200

    Create 1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcpy - encodes a string using rot13
+ * @dest: input string.
+ *
+ * @src: input char.
+ *
+ * @n: input int.
+ *
+ * Return: the pointer to dest.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+{
+dest[i] = src[i];
+}
+return (s);
+}
