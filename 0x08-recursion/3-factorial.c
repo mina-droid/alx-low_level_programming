bea5acd114891737eb81273c3ae7a03d235a3d3b
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-29 02:27:18 +0200

    Create 3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *factorial - encodes a string using rot13
+ * @n: input string.
+ *
+ * Return: the pointer to dest.
+ */
+
+int factorial(int n)
+{
+if (n <= 1)
+{
+return (1);
+}
+return (n * factorial(n - 1));
+}
