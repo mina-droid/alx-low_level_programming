dd59c104937f122df09d618bc21561b022371e21
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-29 02:24:52 +0200

    Create 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_strlen_recursion - encodes a string using rot13
+ * @s: input string.
+ *
+ * Return: the pointer to dest.
+ */
+
+int _strlen_recursion(char *s)
+
+{
+if (*s == '\0')
+{
+return (0);
+}
+
+return (1 + int _strlen_recursion(s + 1));
+
+}
