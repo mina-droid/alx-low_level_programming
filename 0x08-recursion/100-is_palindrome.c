adb3d942c83906a8451c395c9afd20f1afa4e309
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-09-29 17:49:06 +0200

    Create 100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *is_palindrome - encodes a string using rot13
+ * @s: input string.
+ *
+ * Return: the pointer to dest.
+ */
+
+int is_palindrome(char *s)
+
+{
+int len = 
+if (*s == '')
+{
+return (1);
+}
+
+}
