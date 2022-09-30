26857e025751b2d443faf0ce5f18a33559ddbcf2
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-01 01:16:31 +0200

    Create 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main - encodes a string using rot13
+ * @argc: arguments number.
+ *
+ * @argv: array of arguments.
+ *
+ * Return: the pointer to dest.
+ */
+
+int main(int argc, char *argv[])
+{
+printf ("%s", argv[0]);
+}
