4e389f3d453ec69606828a9c28474517832bd25b
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-01 01:33:28 +0200

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,11 +11,14 @@
 
 int main(int argc, char *argv[])
 {
+int n1, n2;
 if (argc < 3)
 {
 printf("Error\n");
 return (1);
 }
-printf("%d\n", argv[1] * argv[2]);
+n1 = argv[1];
+n2 = argv[2];
+printf("%d\n", (n1 * n2));
 return (0);
 }
