4956da1006c0c51443d5ce673bc37338ee2af5cd
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-01 01:23:01 +0200

    Create 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main - encodes a string using rot13
+ * @argc: arguments number.
+ *
+ * @argv: array of arguments.
+ *
+ * Return: the pointer to dest.
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc);
+return (0);
+}
