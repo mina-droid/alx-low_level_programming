fec785d2a5c867fafeada291da3955abbbcedc57
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-01 03:54:14 +0200

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,35 +9,48 @@
  * Return: the pointer to dest.
  */
 
-int main(int argc, char * argv[]) {
+int main (int argc, char *argv[])
+{
   int cent, counter = 0;
-  if (argc != 2) {
-    printf("Error\n");
-    return (1);
-  }
-  cent = atoi(argv[1]);
-  if (cent < 0) {
-    printf("%d\n", 0);
-    return (0);
-  }
-  while (cent > 0) {
-    if (cent >= 25) {
-      cent -= 25;
-      counter++;
-    } else if (cent >= 10) {
-      cent -= 10;
-      counter++;
-    } else if (cent >= 5) {
-      cent -= 5;
-      counter++;
-    } else if (cent >= 2) {
-      cent -= 2;
-      counter++;
-    } else if (cent >= 1) {
-      cent -= 1;
-      counter++;
+  if (argc != 2)
+    {
+      printf ("Error\n");
+      return (1);
     }
-  }
-  printf("%d\n", counter);
+  cent = atoi (argv[1]);
+  if (cent < 0)
+    {
+      printf ("%d\n", 0);
+      return (0);
+    }
+  while (cent > 0)
+    {
+      if (cent >= 25)
+        {
+          cent -= 25;
+          counter++;
+        }
+      else if (cent >= 10)
+        {
+          cent -= 10;
+          counter++;
+        }
+      else if (cent >= 5)
+        {
+          cent -= 5;
+          counter++;
+        }
+      else if (cent >= 2)
+        {
+          cent -= 2;
+          counter++;
+        }
+      else if (cent >= 1)
+        {
+          cent -= 1;
+          counter++;
+        }
+    }
+  printf ("%d\n", counter);
   return (0);
 }
