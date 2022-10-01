13bf9b35478234a91940850c2b6f1a57b252e0fe
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-01 03:56:32 +0200

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -21,8 +21,8 @@ int main (int argc, char *argv[])
   cent = atoi (argv[1]);
   if (cent < 0)
     {
-      printf ("%d\n", 0);
-      return (0);
+     printf("0\n");
+     return (0);
     }
   while (cent > 0)
     {
