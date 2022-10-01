4c1e7a06f5ab50db8c770f5d9dcb25f3f6e8e6a8
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-01 03:57:59 +0200

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,47 +11,47 @@
 
 int main (int argc, char *argv[])
 {
-  int cent, counter;
-  counter = 0;
-  if (argc != 2)
-    {
-      printf ("Error\n");
-      return (1);
-    }
-  cent = atoi (argv[1]);
-  if (cent < 0)
-    {
-     printf("0\n");
-     return (0);
-    }
-  while (cent > 0)
-    {
-      if (cent >= 25)
-        {
-          cent -= 25;
-          counter++;
-        }
-      else if (cent >= 10)
-        {
-          cent -= 10;
-          counter++;
-        }
-      else if (cent >= 5)
-        {
-          cent -= 5;
-          counter++;
-        }
-      else if (cent >= 2)
-        {
-          cent -= 2;
-          counter++;
-        }
-      else if (cent >= 1)
-        {
-          cent -= 1;
-          counter++;
-        }
-    }
-  printf ("%d\n", counter);
-  return (0);
+int cent, counter;
+counter = 0;
+if (argc != 2)
+{
+printf ("Error\n");
+return (1);
+}
+cent = atoi (argv[1]);
+if (cent < 0)
+{
+printf("0\n");
+return (0);
+}
+while (cent > 0)
+{
+if (cent >= 25)
+{
+cent -= 25;
+counter++;
+}
+else if (cent >= 10)
+{
+cent -= 10;
+counter++;
+}
+else if (cent >= 5)
+{
+cent -= 5;
+counter++;
+}
+else if (cent >= 2)
+{
+cent -= 2;
+counter++;
+}
+else if (cent >= 1)
+{
+cent -= 1;
+counter++;
+}
+}
+printf ("%d\n", counter);
+return (0);
 }
