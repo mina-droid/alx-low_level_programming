f447b0e7f2d7a635765a3c6a903bf7a00f19f5f0
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-03 22:44:04 +0200

    Create 3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - function
+ *@width: length of argv
+ *@height: number of argument
+ *Return: Always 0
+ */
+
+int **alloc_grid(int width, int height)
+{
+int i;
+int j;
+int **res;
+if (width == 0 || height == 0 || width < 0 || height < 0)
+{
+return (NULL);
+}
+res = malloc(sizeof(int) *(width * height));
+if (res == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < height; i++)
+{
+for (j = 0; j < width; j++)
+{
+res[i][j] = 0;
+}
+}
+return (res);
+}
