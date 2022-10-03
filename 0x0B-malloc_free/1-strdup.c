3dba52c54b4b3ce7a419ee4d5c85b98bc3bb9d02
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-03 21:43:07 +0200

    Create 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - function
+ *@str: length of argv
+ *Return: Always 0
+ */
+
+char *_strdup(char *str)
+{
+if (str == NULL)
+{
+return (NULL);
+}
+}
