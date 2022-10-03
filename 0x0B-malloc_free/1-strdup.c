3a2423def9b5ec59d10c8e1bc44450698858d71b
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-03 21:47:43 +0200

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,6 +2,25 @@
 #include <stdlib.h>
 
 /**
+ *_strlen_recursion - encodes a string using rot13
+ * @s: input string.
+ *
+ * Return: the pointer to dest.
+ */
+
+int _strlen_recursion(char *s)
+
+{
+if (*s == '\0')
+{
+return (0);
+}
+
+return (1 + _strlen_recursion(s + 1));
+
+}
+
+/**
  * _strdup - function
  *@str: length of argv
  *Return: Always 0
@@ -9,8 +28,18 @@
 
 char *_strdup(char *str)
 {
+int len;
+char *strup;
 if (str == NULL)
 {
 return (NULL);
 }
+len = _strlen_recursion(str);
+strup = malloc(sizeof(char) * len);
+if (strup == NULL)
+{
+return (NULL);
+}
+strup = str;
+return (strup);
 }
