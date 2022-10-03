efe0c320bf3ce415669f230f8da6dab34adf6c6f
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-03 21:58:37 +0200

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,6 +1,20 @@
 #include "main.h"
 #include <stdlib.h>
 
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+
+while (src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
+
+return (dest);
+}
+
 /**
  *_strlen_recursion - encodes a string using rot13
  * @s: input string.
@@ -40,6 +54,6 @@ if (strup == NULL)
 {
 return (NULL);
 }
-strup = str;
+_strcpy(strup, str);
 return (strup);
 }
