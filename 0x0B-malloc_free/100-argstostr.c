94e4956f51764b2386221a14338560b15a8a6cf8
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-04 05:35:27 +0200

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,6 +2,30 @@
 #include <stdlib.h>
 
 /**
+ *_strcmp - convert a string to an integer.
+ * @s1: string one
+ *
+ * @s2: string two
+ *
+ * Return: string
+ */
+int _strcmp(char *s1, char *s2)
+{
+int res = 0;
+while (*s1 != '\0' && *s2 != '\0')
+{
+if (*s1 != *s2)
+{
+res = (*s1 - *s2);
+break;
+}
+s1++;
+s2++;
+}
+return (res);
+}
+
+/**
  *_strlen_recursion - encodes a string using rot13
  * @s: input string.
  *
@@ -80,7 +104,7 @@ size += _strlen_recursion(av[i]);
 res = malloc((sizeof(char) * size) + ac);
 for (i = 1; i < ac; i++)
 {
-if (av[i] == '\"')
+if (_strcmp(av[i], "\"") == 0)
 {
 continue;
 }
