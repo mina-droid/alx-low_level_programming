a1b72f3dceb0bd2f8ff4557db7d1bf2410d87318
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-07 01:29:50 +0200

    Create 100-realloc.c

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ *_realloc - array for prints a string
+ *@ptr: number of memory
+ *@old_size: number of memory
+ *@new_size: number of memory
+ *Return: void
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+void *tmpPtr;
+if (new_size == 0 && ptr != NULL)
+{
+free(ptr);
+return (NULL);
+}
+if (ptr == NULL)
+{
+ptr = malloc(new_size);
+return (ptr);
+}
+if (new_size == old_size)
+{
+return (ptr);
+}
+tmpPtr = malloc(new_size);
+free(ptr);
+ptr = tmpPtr;
+free(tmpPtr);
+return (ptr);
+}
