2a322bd48e9a40a6fb133d0dceccafb9511b881d
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-06 06:45:41 +0200

    Update 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,1 +1,36 @@
+#include <stdlib.h>
+#include "main.h"
+/*
+*array_range-allocate space
+*@min:intger
+*@max:intger
+*/
 
+int *array_range(int min, int max)
+{
+int *ptr;
+int i;
+int size;
+if (min > max)
+{
+return (NULL);
+}
+if (min == max)
+{
+*ptr = min;
+return (ptr);
+}
+size = max - min;
+ptr = malloc(size * sizeof(int));
+if (ptr == NULL)
+{
+return (NULL);
+}
+printf ("size = %d\n", size);
+for (i = 0; i < size + 1 && min <= max; i++)
+{
+ptr[i] = min;
+min++;
+}
+return (ptr);
+}
