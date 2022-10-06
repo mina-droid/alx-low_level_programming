e206c172e2982032b8e09eca823320bde4d04818
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-06 23:47:40 +0200

    Create 1-string_nconcat.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ *string_nconcat - array for prints a string
+ *@s1: number of memory
+ *@s2: number of memory
+ *@n: number of memory
+ *Return: void
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
+}
