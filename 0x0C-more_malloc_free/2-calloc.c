12c950ea4445743a3990ac444bb15e322769c4e2
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-06 06:32:34 +0200

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,1 +1,27 @@
+#include <stdlib.h>
+#include "main.h"
+/*
+*_calloc-allocate space
+*@nmemb:intger
+*@size:intger
+*/
 
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+void *ptr;
+unsigned int i;
+if (nmemb == 0 || size == 0)
+{
+return (NULL);
+}
+ptr = malloc(nmemb * size);
+if (ptr == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < nmemb * size; i++)
+{
+ptr[i] = 0;
+}
+return (ptr);
+}
