f8d268717bfba1c2a59715800f4a55ba59b61db6
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-10 19:35:01 +0200

    Create 2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+*print_dog-dog init
+*@d:dog
+*/
+
+void print_dog(struct dog *d)
+{
+if (d == NULL)
+{
+return;
+}
+if (d->name == 0)
+{
+printf("Name: (nil)\n");
+}
+else
+{
+printf("Name: %s\n", d->name);
+}
+if (d->age == 0)
+{
+printf("Age: (nil)\n");
+}
+else
+{
+printf("Age: %s\n", d->age);
+}
+if (d->owner == 0)
+{
+printf("Owner: (nil)\n");
+}
+else
+{
+printf("Owner: %s\n", d->owner);
+}
+}
