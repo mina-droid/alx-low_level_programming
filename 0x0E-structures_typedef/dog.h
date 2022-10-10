7a241e17f0edb3bd9eec230848e17804bea4d5de
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-10 19:41:29 +0200

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,13 +9,14 @@
  * Description: This struct is for dog
  */
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}struct_dog_t;
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
