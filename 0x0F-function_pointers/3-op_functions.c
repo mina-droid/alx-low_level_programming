e343703e6665ca453a2a5c1d92be5b183801328c
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-12 19:08:32 +0200

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -2,9 +2,10 @@
 #include "calc.h"
 
 /**
-*op_add-dog init
+*op_add-function
 *@a:intger 1
 *@b:intger 2
+*Return: intger
 */
 
 int op_add(int a, int b)
@@ -13,9 +14,10 @@ return (a + b);
 }
 
 /**
-*op_sub-dog init
+*op_sub-function
 *@a:intger 1
 *@b:intger 2
+*Return: intger
 */
 
 int op_sub(int a, int b)
@@ -24,9 +26,10 @@ return (a - b);
 }
 
 /**
-*op_mul-dog init
+*op_mul-function
 *@a:intger 1
 *@b:intger 2
+*Return: intger
 */
 
 int op_mul(int a, int b)
@@ -35,9 +38,10 @@ return (a * b);
 }
 
 /**
-*op_div-dog init
+*op_div-function
 *@a:intger 1
 *@b:intger 2
+*Return: intger
 */
 
 int op_div(int a, int b)
@@ -46,9 +50,10 @@ return (a / b);
 }
 
 /**
-*op_mod-dog init
+*op_mod-function
 *@a:intger 1
 *@b:intger 2
+*Return: intger
 */
 
 int op_mod(int a, int b)
