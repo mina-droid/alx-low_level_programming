f24c04d3a0920f6e954f23e09431dd1eb5374655
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-12 18:01:35 +0200

    Create 3-op_functions.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include <stdlib.h>
+#include "calc.h"
+
+/**
+*op_add-dog init
+*@a:intger 1
+*@b:intger 2
+*/
+
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+*op_sub-dog init
+*@a:intger 1
+*@b:intger 2
+*/
+
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+*op_mul-dog init
+*@a:intger 1
+*@b:intger 2
+*/
+
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+*op_div-dog init
+*@a:intger 1
+*@b:intger 2
+*/
+
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+*op_mod-dog init
+*@a:intger 1
+*@b:intger 2
+*/
+
+int op_mod(int a, int b)
+{
+return (a % b);
+}
