c2b941bfe1338ccd111c1556d5642e98dd8cca9e
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-12 19:34:37 +0200

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,37 +1,46 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include "calc.h"
+#include "3-calc.h"
 
 /**
-*main-function
-*@argc:intger 1
-*@argv:array of strings
-*/
-
+ * main - program that perfroms simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
-int num1, num2, res;
-int (*reqfunc)(int, int);
-if (argc != 4)
-{
-printf("Error\n");
-exit(98);
-}
-num1 = atoi(argv[1]);
-num2 = atoi(argv[3]);
-if ((*argv[2] == '%' && num2 == 0) || (*argv[2] == '/' && num2 == 0))
-{
-printf("Error\n");
-exit(100);
-}
-reqfunc = get_op_func(argv[2]);
-if (!reqfunc)
-{
-printf("Error\n");
-exit(99);
-}
-res = reqfunc(num1, num2);
-printf("%d\n", res);
-return (0);
+	int arg1, arg2, result;
+	char o;
+	int (*func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
+
+	func = get_op_func(argv[2]);
+
+	if (!func)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	o = *argv[2];
+
+	if ((o == '/' || o == '%') && arg2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = func(arg1, arg2);
+
+	printf("%d\n", result);
+
+	return (0);
 }
