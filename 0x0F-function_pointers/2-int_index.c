d59f279116dbbb02457507b99ceb809993fe9009
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-12 17:45:15 +0200

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,13 +15,13 @@ if (array == NULL || cmp == NULL)
 {
 return (-1);
 }
-if (size <=0)
+if (size <= 0)
 {
 return (-1);
 }
 for (i = 0; i < size; i++)
 {
-if (cmp(array[i]))
+if (cmp(array[i]) != 0)
 {
 return (i);
 }
