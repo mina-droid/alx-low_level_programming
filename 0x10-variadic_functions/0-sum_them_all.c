2e431ade42e4e56a0709629cf07fd6bddd803639
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-13 11:41:26 +0200

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,5 +8,18 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-
+va_list argL;
+unsigned int i;
+int sum = 0;
+if (n == 0)
+{
+return (0);
+}
+va_start(argL, n);
+for (i = 0; i < n; i++)
+{
+sum += va_arg(argL, int);
+}
+va_end(argL);
+return (sum);
 }
