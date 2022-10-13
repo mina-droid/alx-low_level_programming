473d0fe28114079511cf5c517c60af651d6f910f
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-13 12:00:36 +0200

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,10 +11,17 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 va_list argL;
 unsigned int i;
+
 va_start(argL, n);
+
 for (i = 0; i < n; i++)
 {
-_putchar('h');
+if ((i != n - 1) && separator != NULL)
+{
+printf("%d%s ", va_arg(argL, int), separator);
+}   
+printf("%d", va_arg(argL, int));
 }
+printf("\n");
 va_end(argL);
 }
