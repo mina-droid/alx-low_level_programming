312f98d474a7ef4fdd03a57defab28fcba426b56
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-13 13:11:28 +0200

    Update 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,34 +1,51 @@
 #include "variadic_functions.h"
 
 /**
-*print_strings-sum all args
-*@n:number
-*@separator:string
-*Return: sum
-*/
+ * print_all - prints anything.
+ * @format: a list of types of arguments passed to the function.
+ *
+ * Return: no return.
+ */
+void print_all(const char * const format, ...)
+{
+	va_list valist;
+	unsigned int i = 0, j, c = 0;
+	char *str;
+	const char t_arg[] = "cifs";
 
-void print_strings(const char *separator, const unsigned int n, ...)
-{
-va_list argL;
-unsigned int i;
-char *str;
-va_start(argL, n);
-for (i = 0; i < n; i++)
-{
-str = va_arg(argL, char *);
-if (str != NULL)
-{
-printf("%s", str);
-}
-else
-{
-printf("(nil)");
-}
-if (separator && i < n - 1)
-{
-printf("%s", separator);
-}
-}
-printf("\n");
-va_end(argL);
+	va_start(valist, format);
+	while (format && format[i])
+	{
+		j = 0;
+		while (t_arg[j])
+		{
+			if (format[i] == t_arg[j] && c)
+			{
+				printf(", ");
+				break;
+			} j++;
+		}
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(valist, int)), c = 1;
+			break;
+		case 'i':
+			printf("%d", va_arg(valist, int)), c = 1;
+			break;
+		case 'f':
+			printf("%f", va_arg(valist, double)), c = 1;
+			break;
+		case 's':
+			str = va_arg(valist, char *), c = 1;
+			if (!str)
+			{
+				printf("(nil)");
+				break;
+			}
+			printf("%s", str);
+			break;
+		} i++;
+	}
+	printf("\n"), va_end(valist);
 }
