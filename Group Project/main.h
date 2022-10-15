f234989ec2d30f21cfab1a0e23b09a0005774184
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-15 20:20:28 +0200

    Update main.h

--- Group Project/main.h
+++ Group Project/main.h
@@ -4,6 +4,20 @@
 #include<stdlib.h>
 #include<stdarg.h>
 
+/**
+ * struct printer - A new struct type defining a printer.
+ * @symbol: A symbol representing a data type.
+ * @print: A function pointer to a function that prints
+ *         a data type corresponding to symbol.
+ */
+typedef struct printer
+{
+	char *symbol;
+	void (*print)(va_list arg);
+
+} printer_t;
+
+int _putchar(char c);
 int _printf(const char *format, ...);
 
 #end
