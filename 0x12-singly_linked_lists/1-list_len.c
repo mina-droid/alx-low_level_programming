9461dd2da08543b93470f5b835e317aa22f5b963
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-20 17:09:27 +0200

    Create 1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - Prints all the elements of a list_t list.
+ * @h: The list_t list.
+ *
+ * Return: The number of nodes in h.
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+  printf("%d", count);
+	return (count);
+}
