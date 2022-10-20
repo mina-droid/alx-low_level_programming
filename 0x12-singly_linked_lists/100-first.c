87c4f26f1712fcfdc3446b92ee2d99637baf37ef
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-20 20:28:38 +0200

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,4 +1,11 @@
 #include "lists.h"
 
 /**
-* first - prin 
+ * first - Prints all the elements of a list_t list.
+ * Return: void.
+ */
+
+void first(void)
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
