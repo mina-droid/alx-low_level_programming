c8d8544917287784f5cf3979b74540e4bc4a69ec
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-20 16:57:05 +0200

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,20 +6,16 @@
  */
 size_t print_list(const list_t *h)
 {
-size_t count;
-count = 0;
+size_t nelem;
+nelem = 0;
 while (h != NULL)
 {
 if (h->str == NULL)
-{
-printf("[0] (nil)\n");
-}
+printf("[%d] %s\n", 0, "(nil)");
 else
-{
 printf("[%d] %s\n", h->len, h->str);
-}
-count++;
 h = h->next;
+nelem++;
 }
-return (count);
+return (nelem);
 }
