beb4737a9a018af22cf372da15c2e2e5c14a5e34
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-20 16:34:56 +0200

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,20 +3,16 @@
 /**
  *_strlen_recursion - encodes a string using rot13
  * @s: input string.
- *
- * Return: the pointer to dest.
+  * Return: the pointer to dest.
  */
 
 int _strlen_recursion(char *s)
-
 {
 if (*s == '\0')
 {
 return (0);
 }
-
 return (1 + _strlen_recursion(s + 1));
-
 }
 
 /**
