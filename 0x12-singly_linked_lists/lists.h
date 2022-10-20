fee1d1c9d7e60c32c1963ae0de1912142f7f835a
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-20 15:41:36 +0200

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,20 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+typedef struct lists *listPtr;
+
+/**
+*lists- list struct
+*@value: data
+*@next: pointer to next node
+*/
+
+struct lists {
+  int value;
+  listPtr *next;
+};
+typedef struct lists list_t;
+
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
