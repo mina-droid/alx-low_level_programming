16a2102ccf2158dfae92b0daf0d53c7c64006bd1
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-20 19:17:06 +0200

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,25 @@
 #include "lists.h"
 
 /**
+ *_strlen_recursion - encodes a string using rot13
+ * @s: input string.
+ *
+ * Return: the pointer to dest.
+ */
+
+int _strlen_recursion(char *s)
+
+{
+if (*s == '\0')
+{
+return (0);
+}
+
+return (1 + _strlen_recursion(s + 1));
+
+}
+
+/**
  * add_node - Prints all the elements of a list_t list.
  * @head: The list_t list.
  *
@@ -12,14 +31,15 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t * newNode;
-  char *newStr;
-  newStr = strdup(str);
-  
-  
-	while (h != NULL)
-	{
-		count++;
-		h = h->next;
-	}
-	return (count);
+  	char *newStr;
+  	unsigned int newLen;
+	newNode = malloc(sizeof(list_t));
+	newStr = strdup(str);
+	newLen = _strlen_recursion(newStr);
+  	newNode->str = newStr;
+  	newNode->len = newLen;
+  	newNode->next = *head;
+	*head = newNode;
+
+	return (newNode);
 }
