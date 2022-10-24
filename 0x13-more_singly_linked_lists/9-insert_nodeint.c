fca7dde305c7cc2ac3e21d043dae8049d79857a4
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-24 22:50:39 +0200

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,25 +1,6 @@
 #include "lists.h"
 
 /**
- * listint_len - Prints all the elements of a list_t list.
- * @h: The list_t list.
- *
- * Return: The number of nodes in h.
- */
-
-size_t listint_len(const listint_t *h)
-{
-	size_t count = 0;
-
-	while (h != NULL)
-	{
-		count++;
-		h = h->next;
-	}
-	return (count);
-}
-
-/**
  * insert_nodeint_at_index - Prints all the elements of a list_t list.
  * @head: The list_t list.
  * @idx: index
@@ -29,7 +10,6 @@ size_t listint_len(const listint_t *h)
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-size_t len;
 unsigned int i = 0;
 listint_t *newNode;
 listint_t *itr = *head;
@@ -41,18 +21,12 @@ if (*head == NULL)
 {
 return (NULL);
 }
-len = listint_len(*head);
-if (idx >= len)
-{
-return (NULL);
-}
 newNode = malloc(sizeof(listint_t));
 if (newNode == NULL)
 {
 return (NULL);
 }
 newNode->n = n;
-newNode->next = NULL;
 if (idx == 0)
 {
 newNode->next = itr;
@@ -61,6 +35,10 @@ return (newNode);
 }
 while (i < (idx - 1))
 {
+if (itr == NULL || itr->next == NULL)
+{
+return (NULL);
+}
 itr = itr->next;
 i++;
 }
