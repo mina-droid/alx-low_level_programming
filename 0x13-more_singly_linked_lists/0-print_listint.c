6c6fc718204deaffc781bc89f1f5ab3c2f480e9e
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-24 18:04:39 +0200

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,19 +1,20 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a linked list
- * @h: linked list of type listint_t to print
+ * print_listint - Prints all the elements of a list_t list.
+ * @h: The list_t list.
  *
- * Return: number of nodes
+ * Return: The number of nodes in h.
  */
+
 size_t print_listint(const listint_t *h)
 {
-size_t num = 0;
-while (h)
-{
-printf("%d\n", h->n);
-num++;
-h = h->next;
-}
-return (num);
+	size_t count = 0;
+
+	printf("%d\n", h->n);
+	count++;
+	h = h->next;
+	}
+
+	return (count);
 }
