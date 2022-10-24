126c99ca605146ca9e1d78c55187c2ff4e4ad10b
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-24 17:14:16 +0200

    Create 2-add_nodeint.c

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * add_node - Prints all the elements of a list_t list.
+ * @head: The list_t list.
+ *
+ * @str: The string.
+ *
+ * Return: The new node.
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n);
+{
+listint_t *newNode;
+int newN;
+unsigned int newLen;
+newNode = malloc(sizeof(listint_t));
+if (newNode == NULL)
+{
+return (NULL);
+}
+newN = n;
+newNode->n = newN;
+newNode->next = *head;
+*head = newNode;
+return (newNode);
+}
