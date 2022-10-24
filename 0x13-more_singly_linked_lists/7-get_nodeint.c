50513b8afd9f22fe6f7636eea08a3863659bfe0a
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-24 21:39:14 +0200

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,6 +1,25 @@
 #include "lists.h"
 
 /**
+ * listint_len - Prints all the elements of a list_t list.
+ * @h: The list_t list.
+ *
+ * Return: The number of nodes in h.
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
+
+/**
  * get_nodeint_at_index - Prints all the elements of a list_t list.
  * @head: The list_t list.
  * @index: index
