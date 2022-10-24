919675f55a13af6384db480678f3f5e495591062
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-24 23:08:40 +0200

    Create 10-delete_nodeint.c

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - Prints all the elements of a list_t list.
+ * @head: The list_t list.
+ * @index: index
+ * Return: void.
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+{
+unsigned int i = 0;
+listint_t *itr = *head, *temp;
+if (idx == 0)
+{
+*head = (*head)->next;
+free(itr);
+return (1);
+}
+while (i < (idx - 1))
+{
+if (itr == NULL || itr->next == NULL)
+{
+return (-1);
+}
+itr = itr->next;
+i++;
+}
+temp = itr->next;
+itr->next = temp->next;
+free(temp);
+return (1);
+}
