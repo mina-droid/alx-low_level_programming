2982a79f087db7525c8f2d21a3ea036358b1339c
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-24 23:11:59 +0200

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,6 +11,10 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 unsigned int i = 0;
 listint_t *itr = *head, *temp;
+if (itr == NULL)
+{
+return (-1);
+}
 if (index == 0)
 {
 *head = (*head)->next;
