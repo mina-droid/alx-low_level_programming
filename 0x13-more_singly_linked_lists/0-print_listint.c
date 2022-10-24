109e74e64913ccdb6f4df404b3b0869b3a4f2cf1
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-24 18:07:22 +0200

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 
 /**
- * print_list - Prints all the elements of a list_t list.
+ * print_listint - Prints all the elements of a list_t list.
  * @h: The list_t list.
  *
  * Return: The number of nodes in h.
  */
 
-size_t print_list(const list_t *h)
+size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
