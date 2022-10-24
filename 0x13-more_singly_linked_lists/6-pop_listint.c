5f280bde774b94e7cf2e1ea0101ca28cddfbafa1
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-24 20:50:03 +0200

    Create 6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Prints all the elements of a list_t list.
+ * @head: The list_t list.
+ *
+ * Return: void.
+ */
+
+int pop_listint(listint_t **head)
+{
+int nodeN;
+if (head == NULL)
+{
+return (0);
+}
+nodeN = *head->n;
+head = (*head)->next;
+*head = NULL;
+return (nodeN);
+}
