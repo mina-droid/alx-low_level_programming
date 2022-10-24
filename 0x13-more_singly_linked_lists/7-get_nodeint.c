e8a1b0abb7300f9d1d656e8dd7894c9bdf0bdb96
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-24 21:35:30 +0200

    Create 7-get_nodeint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - Prints all the elements of a list_t list.
+ * @head: The list_t list.
+ * @index: index
+ * Return: void.
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+size_t len;
+unsigned int i = 0;
+if (head == NULL)
+{
+return (NULL);
+}
+len = listint_len(head);
+if (index >= len)
+{
+return (NULL);
+}
+while (i < index)
+{
+head = (*head)->next;
+}
+return (head);
+}
