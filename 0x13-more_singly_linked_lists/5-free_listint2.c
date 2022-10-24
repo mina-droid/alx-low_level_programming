e53af0220824231d8278a121910845c344fc7d99
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-24 20:34:23 +0200

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,14 +12,13 @@ void free_listint2(listint_t **head)
 listint_t *temp;
 if (head == NULL)
 {
-return (NULL);
+return;
 }
 while (*head != NULL)
 {
-temp = *head;
-*head = (*head)->next;
-free(*temp);
-}
+temp = (*head)->next;
 free(*head);
+*head = temp;
+}
 head = NULL;
 }
