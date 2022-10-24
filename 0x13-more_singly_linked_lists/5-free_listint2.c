7d4851fae674d8809bf1d4a3bda08c5796d67411
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-24 20:24:34 +0200

    Create 5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Prints all the elements of a list_t list.
+ * @head: The list_t list.
+ *
+ * Return: void.
+ */
+
+void free_listint2(listint_t **head)
+{
+listint_t **temp;
+while (*head != NULL)
+{
+*temp = *head;
+*head = *head->next;
+free(*temp);
+}
+free(*head);
+*head = NULL;
+}
