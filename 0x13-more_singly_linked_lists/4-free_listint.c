cdefacad98544fd7b965bc0cb3f6fd5210748862
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-24 17:50:41 +0200

    Create 4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint - Prints all the elements of a list_t list.
+ * @head: The list_t list.
+ *
+ * Return: void.
+ */
+
+void free_listint(listint_t *head)
+{
+list_t *temp;
+while (head != NULL)
+{
+temp = head;
+head = head->next;
+free(temp);
+}
+free(head);
+}
