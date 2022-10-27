c9cd5a956ead36dbc472c1c523c4019e9fe09f00
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2022-10-27 19:20:27 +0200

    Create 0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - Prints all the elements of a listint_t list.
+ * @b: A pointer to the head of the list_t list.
+ *
+ * Return: The number of nodes in the list_t list.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+if (b == NULL)
+{
+return (0);
+}
+}
